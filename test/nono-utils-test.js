0f9132723645529988119b32811758119ea93635
Simon Robert <simon.robert@cgi.com>
2025-01-01 02:39:01 -0500

    Refactoring to make modules

---
+++ test/nono-utils-test.js
@@ -0,0 +1,56 @@
+import * as nono from '../js/util/nono-utils.js';
+
+testSolve1();
+testSolve2();
+testGen();
+
+function testGen() {
+    nono.generateNonogramLink(5, 10, 'acbdg');
+}
+
+function testSolve1() {
+    let horHints = [
+        [2,2],
+        [4,4],
+        [1,2,2],
+        [5,2,1],
+        [1,2,4],
+        [4,1,1],
+        [1,1,3],
+        [5,1],
+    ];
+
+    let verHints = [
+        [6],
+        [2,1,1,1],
+        [2,5],
+        [5,1],
+        [3,1],
+        [1],
+        [1,5],
+        [2,2,1],
+        [3,3],
+        [3]
+    ];
+
+    nono.solveNonogram(horHints, verHints);
+}
+
+function testSolve2() {
+    let horHints = [
+        [1],
+        [1,2],
+        [1],
+        [5]
+    ];
+
+    let verHints = [
+        [1,1],
+        [1,1],
+        [1],
+        [1,1],
+        [3]
+    ];
+
+    nono.solveNonogram(horHints, verHints);
+}
